79e132c19077ac98c41073c1887b3e5b3b6b604f
inte <54321156+inte-nent@users.noreply.github.com>
2024-09-12 01:15:43 +0900

    Playerのコーディング
    アニメーション，Inputについてのコーディング

--- Assets/Scenes/Muramatsu/Scripts/Player/PlayerMovement.cs
+++ Assets/Scenes/Muramatsu/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-         
+        private Animator player_animator = default;
 
         private void Awake()
         {
+            player_animator = GetComponent<Animator>();
+        }
+
+        public void OnWalk(InputAction.CallbackContext context)
+        {
+            Debug.Log("walking now");
+            player_animator.SetBool("is_moving", true);
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            Debug.Log("jumping now");
+            player_animator.SetTrigger("jump_trigger");
         }
     }
 }
