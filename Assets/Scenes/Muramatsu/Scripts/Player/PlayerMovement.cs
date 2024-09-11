eb6705f1882bf65423fe895f767203ae21cda268
inte <54321156+inte-nent@users.noreply.github.com>
2024-09-11 22:29:37 +0900

    PlayerInputのActionMapを編集

---
+++ Assets/Scenes/Muramatsu/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+         
+
+        private void Awake()
+        {
+        }
+    }
+}
