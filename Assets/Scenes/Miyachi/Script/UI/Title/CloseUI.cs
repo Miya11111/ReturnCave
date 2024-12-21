69437e68f6f1665221afbcd377ce2b8ede0c4ba2
inte <54321156+inte-nent@users.noreply.github.com>
2024-12-21 17:42:57 +0900

    ステージ拡張，ボタンの挙動設定など，宮地指示の元行った本日の作業分

--- Assets/Scenes/Miyachi/Script/UI/Title/CloseUI.cs
+++ Assets/Scenes/Miyachi/Script/UI/Title/CloseUI.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CloseUI : MonoBehaviour
 {
     [SerializeField]
     private GameObject configObj;
 
+    [SerializeField]
+    GameObject gameObj;
+
     public void OnClick(){
+
+        // [せってい]ボタンのオブジェクトを選択
+        EventSystem.current.SetSelectedGameObject(gameObj);
+
         configObj.SetActive(false);
     }
 }
--- Assets/Scenes/Miyachi/Script/UI/Title/OpenUI.cs
+++ Assets/Scenes/Miyachi/Script/UI/Title/OpenUI.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class OpenUI : MonoBehaviour
 {
     [SerializeField]
     private GameObject configObj;
- public void OnClick(){
+
+    [SerializeField]
+    GameObject gameObj;
+
+    public void OnClick(){
     configObj.SetActive(true);
- }
+
+        // [にほんご]ボタンのオブジェクトを選択
+        EventSystem.current.SetSelectedGameObject(gameObj);
+    }
 }
